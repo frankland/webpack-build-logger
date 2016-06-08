4e2d766aabe22b31987bc44054a87ff0a18592c2
Valerii <v.sorokobatko@plarium.com>
2016-06-08 14:01:07 +0300

    add auto logger

--- src/webpack-build-logger.js
+++ src/webpack-build-logger.js
@@ -1,22 +1,25 @@
 import BaseEvents from 'base-events';
+import chalk from 'chalk';
 
-let _counter = Symbol('counter');
+const _counter = Symbol('counter');
+const _options = Symbol('options');
 
 export default class WebpackLogPlugin extends BaseEvents {
-  constructor() {
+  constructor(options) {
     super();
+    this[_options] = options;
     this[_counter] = 1;
   }
 
   apply(compiler) {
     compiler.plugin('done', (stats) => {
-      let counter = this[_counter]++;
+      const counter = this[_counter]++;
 
-      let time =  (stats.endTime - stats.startTime) / 1000;
-      let scripts = stats.compilation.fileDependencies;
+      const time =  (stats.endTime - stats.startTime) / 1000;
+      const scripts = stats.compilation.fileDependencies;
 
-      let warnings = stats.compilation.warnings;
-      let errors = stats.compilation.errors;
+      const warnings = stats.compilation.warnings;
+      const errors = stats.compilation.errors;
 
       if (Array.isArray(errors) && errors.length) {
         this.emit('build.error', { errors });
@@ -28,10 +31,41 @@ export default class WebpackLogPlugin extends BaseEvents {
           warnings
         });
       }
+
+      if (this[_options].logEnabled) {
+        const colorize = chalk.green;
+        const log = this[_options].log || console.log;
+
+        let message = `${colorize('#' + counter)} application was packed. Elapsed time ${colorize(time + 's')}. `;
+        message += `Number of scripts ${colorize(scripts.length)}`;
+
+        log(message);
+
+        if (warnings && !!warnings.length) {
+          const warningColorize = chalk.cyan;
+
+          log('------------------');
+          log(warningColorize('*** WARNINGS ***'));
+          for (var warning of warnings) {
+            log(`at ${warningColorize(warning.module.issuer)}`);
+            log(`requested "${warningColorize(warning.module.rawRequest)}" ("${warningColorize(warning.module.userRequest)}")`);
+            log(warning.message.replace(/(\r\n|\n|\r)/gm, ' '));
+          }
+
+          log('------------------');
+        }
+      }
     });
 
     compiler.plugin('invalid', () => {
       this.emit('build.start');
+
+      if (this[_options].logEnabled) {
+        const colorize = chalk.green;
+        const log = this[_options].log || console.log;
+
+        log(colorize('rebuild application'));
+      }
     });
   }
 }
